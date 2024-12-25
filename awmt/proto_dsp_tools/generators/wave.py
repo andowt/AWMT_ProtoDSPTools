e9bb6c1abcb4ece7aff2404f5f093dc00ef219df
Andy West <andy.west@focusrite.com>
2024-12-25 15:31:54 +0000

    Implement sine, square and triangle generators

--- awmt/proto_dsp_tools/generators/wave.py
+++ awmt/proto_dsp_tools/generators/wave.py
@@ -15,12 +15,40 @@ class Wave():
         self.phase_rad = (phase_deg * np.pi) / 360
         self.wave_type = wave_type
 
+        if frequency_hz < 0:
+            raise ValueError(f"Negative frequency requested {frequency_hz}")
+        if not 0 <= phase_deg <= 360:
+            raise ValueError(f"Invalid phase requested: {phase_deg}")
+        if not 0 <= amplitude_normal <= 1:
+            raise ValueError(f"Invalid amplitude requested: {amplitude_normal}")
+
 class WaveGenerator(GeneratorBase):
     def __init__(self, wave_list: list[Wave]):
         self.wave_list = wave_list
-        
+
     def generate(self, duration_ms: float, sample_rate_hz: int) -> list:
-        t = np.linspace(0, duration_ms, int(sample_rate_hz * duration_ms))
-        freqs = np.linspace(self.start_freq, self.end_freq, len(t))
-        signal = np.sin(2 * np.pi * freqs * t)
+
+        duration_s = duration_ms / 1000.0
+        t = np.linspace(0, duration_s, int(sample_rate_hz * duration_s), endpoint=False)
+        signal = np.zeros_like(t)
+                # Generate and mix each wave
+        for wave in self.wave_list:
+            base_sine = np.sin(2 * np.pi * wave.frequency_hz * t + wave.phase_rad)
+            if wave.wave_type == WaveType.sine:
+                wave_signal = wave.amplitude_normal * base_sine
+            elif wave.wave_type == WaveType.triangle:
+                # arcsin is used to generate a triangle wave as it linearises the waveform
+                # 0:1 is mapped to 0:pi/2
+                # 1:0 is mapped to pi/2:0
+                # -1:0 is mapped to -pi/2:0
+                # 0:-1 is mapped to 0:-pi/2
+                wave_signal = wave.amplitude_normal * (2 / np.pi) * np.arcsin(base_sine)
+            elif wave.wave_type == WaveType.square:
+                # np.sign used to generate square wave mapping positive values to 1 and negative to -1
+                wave_signal = wave.amplitude_normal * np.sign(base_sine)
+            else:
+                raise ValueError(f"Unsupported wave type: {wave.wave_type}")
+            
+            signal += wave_signal
+
         return signal.tolist()
